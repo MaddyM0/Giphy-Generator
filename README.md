b7b9b1e90614f28fb1b603a4b25ed2530d178637
Maddy Murguia <maddymurguia@Maddys-Air.hsd1.ca.comcast.net>
2020-07-15 09:39:11 -0700

    Wireframe and basic js

---
+++ README.md
@@ -0,0 +1,1 @@
+A Giphy inspired project where users can input their search requests and have their giphy results show up on the false-computer screen with API from Giphy.
---
+++ js/script.js
@@ -0,0 +1,33 @@
+let giphyData, userInput;
+
+const $input = $("input[type='text']");
+const $gifs = $("#gifsctr");
+
+$("form").on("submit", handleGetData);
+function handleGetData(event) {
+    event.preventDefault();
+    userInput = $input.val();
+    $.ajax({
+        url:"https://api.giphy.com/v1/gifs/search?q="+ userInput +"&api_key=D9y8qvA9zJMc0yDk19y0TEBysPBNP79N" // www. taken off
+    }).then(
+        (data) => {
+            console.log("data is", data)
+            giphyData = data.data[0];
+            render();
+        },
+        (error) => {
+            console.log("error is", error)
+        }
+    )
+}
+function generateHTML() {
+        
+        return `<span class="gifs">
+        <video autoplay src="${giphyData.images.looping.mp4}"></video>
+        </span>`
+    
+}
+
+function render() {
+    $gifs.html(generateHTML())
+}
